fbe84d05a770fd189090c4c62720fa15d49a3a6f
contact-andy <meetandargachew@gmail.com>
2023-03-03 11:40:12 +0300

    invoices page added

--- src/scenes/team/index.jsx
+++ src/scenes/team/index.jsx
@@ -10,91 +10,6 @@ import Header from "../../components/Header";
 const Team =() =>{
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
-
-//     const rows = [
-    
-//   {
-//     id: 1,
-//     name: "Jon Snow",
-//     email: "jonsnow@gmail.com",
-//     age: 35,
-//     phone: "(665)121-5454",
-//     access: "admin",
-//   },
-//   {
-//     id: 2,
-//     name: "Cersei Lannister",
-//     email: "cerseilannister@gmail.com",
-//     age: 42,
-//     phone: "(421)314-2288",
-//     access: "manager",
-//   },
-//   {
-//     id: 3,
-//     name: "Jaime Lannister",
-//     email: "jaimelannister@gmail.com",
-//     age: 45,
-//     phone: "(422)982-6739",
-//     access: "user",
-//   },
-//   {
-//     id: 4,
-//     name: "Anya Stark",
-//     email: "anyastark@gmail.com",
-//     age: 16,
-//     phone: "(921)425-6742",
-//     access: "admin",
-//   },
-//   {
-//     id: 5,
-//     name: "Daenerys Targaryen",
-//     email: "daenerystargaryen@gmail.com",
-//     age: 31,
-//     phone: "(421)445-1189",
-//     access: "user",
-//   },
-//   {
-//     id: 6,
-//     name: "Ever Melisandre",
-//     email: "evermelisandre@gmail.com",
-//     age: 150,
-//     phone: "(232)545-6483",
-//     access: "manager",
-//   },
-//   {
-//     id: 7,
-//     name: "Ferrara Clifford",
-//     email: "ferraraclifford@gmail.com",
-//     age: 44,
-//     phone: "(543)124-0123",
-//     access: "user",
-//   },
-//   {
-//     id: 8,
-//     name: "Rossini Frances",
-//     email: "rossinifrances@gmail.com",
-//     age: 36,
-//     phone: "(222)444-5555",
-//     access: "user",
-//   },
-//   {
-//     id: 9,
-//     name: "Harvey Roxie",
-//     email: "harveyroxie@gmail.com",
-//     age: 65,
-//     phone: "(444)555-6239",
-//     access: "admin",
-//   },
-//     ];
-
-    // const columns = [
-    //  {field: "id", headerName:"ID"},
-    //     {field: "name", headerName:"Name", flex:1,cellClassName:"name-column--cell"},
-    //     {field: "age", headerName:"Age", type:"number",headerAlign:"left",align:"left"},
-    //     {field: "phone", headerName:"Phone Number", flex:1},
-    //     {field: "email", headerName:"Email", flex:1},
-    //     {field: "access", headerName:"Access Level", flex:1 },
-    // ];
     const columns = [
         {field: "id", headerName:"ID"},
         {field: "name", headerName:"Name", flex:1,cellClassName:"name-column--cell"},
@@ -132,8 +47,8 @@ const Team =() =>{
                 <Header title="TEAM" subTitle="Our teams"/>
             </Box>
             <Box
-                m="40px 0 0 0"
-                height="45vh" 
+                m="0px 0 0 0"
+                height="75vh" 
                 sx={{
                     "& .MuiDataGrid-root":{
                         border:'none',
