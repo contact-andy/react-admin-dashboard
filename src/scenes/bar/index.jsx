3aa32fd4654c85a3b25639b1836fc3066f918bd4
contact-andy <meetandargachew@gmail.com>
2023-03-03 19:20:09 +0300

    Bar Chart added

--- src/scenes/bar/index.jsx
+++ src/scenes/bar/index.jsx
@@ -1,20 +1,15 @@
 import { Box } from "@mui/material";
+import BarChart from "../../components/BarChart";
 import Header from "../../components/Header";
 
 const Bar =() =>{
     return (
-        <Box
-            m="20px"
-        >
-            <Box 
-                display="flex"
-                justifyContent="space-between"
-                alignItems="center"
-            >    
-                <Header 
-                    title="BAR" 
-                    subTitle="Bar Chart"
-                />
+        <Box m="20px">
+            <Box display="flex" justifyContent="space-between" alignItems="center">    
+                <Header title="Bar Chart" subTitle="Simple Bar Chart"/>
+            </Box>
+            <Box height="75vh">
+                <BarChart/>
             </Box>
         </Box>
     )
