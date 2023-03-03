9429254cfc87a19caf58e47b3ad5ae19b96e2994
contact-andy <meetandargachew@gmail.com>
2023-03-03 18:50:28 +0300

    Frequently Asked Questions Page

--- src/scenes/faq/index.jsx
+++ src/scenes/faq/index.jsx
@@ -1,21 +1,56 @@
-import { Box } from "@mui/material";
+import { Box, Typography, useTheme } from "@mui/material";
+import {tokens} from "../../theme";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import Header from "../../components/Header";
 
 const FAQ =() =>{
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    
     return (
-        <Box
-            m="20px"
-        >
-            <Box 
-                display="flex"
-                justifyContent="space-between"
-                alignItems="center"
-            >    
-                <Header 
-                    title="FAQ" 
-                    subTitle="Frequently Asked Questions"
-                />
+        <Box m="20px">
+            <Box display="flex" justifyContent="space-between" alignItems="center">    
+                <Header title="FAQ" subTitle="Frequently Asked Questions Page"/>
             </Box>
+            <Accordion>
+                <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
+                    <Typography color={colors.greenAccent[500]} variant='h5'>
+                       An Important Question-1
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                       Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Donec sollicitudin molestie malesuada. Donec sollicitudin molestie malesuada. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Cras ultricies ligula sed magna dictum porta.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion>
+                 <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
+                    <Typography color={colors.greenAccent[500]} variant='h5'>
+                       An Important Question-2
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                       Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Donec sollicitudin molestie malesuada. Donec sollicitudin molestie malesuada. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Cras ultricies ligula sed magna dictum porta.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion defaultExpanded>
+                 <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
+                    <Typography color={colors.greenAccent[500]} variant='h5'>
+                       An Important Question-3
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                       Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Donec sollicitudin molestie malesuada. Donec sollicitudin molestie malesuada. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Cras ultricies ligula sed magna dictum porta.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
         </Box>
     )
 }
