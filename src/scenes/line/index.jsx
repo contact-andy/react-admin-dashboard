fa772646996f56cc303a6a5d11e883d542d0223c
contact-andy <meetandargachew@gmail.com>
2023-03-04 08:54:27 +0300

    Pie Chart added

--- src/scenes/line/index.jsx
+++ src/scenes/line/index.jsx
@@ -1,20 +1,15 @@
 import { Box } from "@mui/material";
+import LineChart from "../../components/LineChart";
 import Header from "../../components/Header";
 
 const Line =() =>{
     return (
-        <Box
-            m="20px"
-        >
-            <Box 
-                display="flex"
-                justifyContent="space-between"
-                alignItems="center"
-            >    
-                <Header 
-                    title="LINE CHART" 
-                    subTitle="Line chart"
-                />
+        <Box m="20px">
+            <Box display="flex" justifyContent="space-between" alignItems="center">    
+                <Header title="LINE Chart" subTitle="Simple Line Chart"/>
+            </Box>
+            <Box height="75vh">
+                <LineChart/>
             </Box>
         </Box>
     )
--- src/scenes/pie/index.jsx
+++ src/scenes/pie/index.jsx
@@ -1,20 +1,15 @@
 import { Box } from "@mui/material";
+import PieChart from "../../components/PieChart";
 import Header from "../../components/Header";
 
 const Pie =() =>{
     return (
-        <Box
-            m="20px"
-        >
-            <Box 
-                display="flex"
-                justifyContent="space-between"
-                alignItems="center"
-            >    
-                <Header 
-                    title="PIE CHART" 
-                    subTitle="Pie Chart"
-                />
+        <Box m="20px">
+            <Box display="flex" justifyContent="space-between" alignItems="center">    
+                <Header title="PIE Chart" subTitle="Simple PIE Chart"/>
+            </Box>
+            <Box height="75vh">
+                <PieChart/>
             </Box>
         </Box>
     )
