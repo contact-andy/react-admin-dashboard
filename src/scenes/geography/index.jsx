50541b1915dfa0087acb6bce2b2df19c95fa9cdb
contact-andy <meetandargachew@gmail.com>
2023-03-04 09:38:25 +0300

    sample geography chart added

--- src/scenes/geography/index.jsx
+++ src/scenes/geography/index.jsx
@@ -1,20 +1,19 @@
+import { useTheme } from "@mui/material";
+import { tokens } from "../../theme";
 import { Box } from "@mui/material";
+import GeographyChart from "../../components/GeographyChart";
 import Header from "../../components/Header";
 
 const Geography =() =>{
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode)
     return (
-        <Box
-            m="20px"
-        >
-            <Box 
-                display="flex"
-                justifyContent="space-between"
-                alignItems="center"
-            >    
-                <Header 
-                    title="GEOGRAPHY" 
-                    subTitle="Geography"
-                />
+        <Box m="20px">
+            <Box display="flex" justifyContent="space-between" alignItems="center">    
+                <Header title="Geography Chart" subTitle="Simple Geography Chart"/>
+            </Box>
+            <Box height="75vh" border={` 1px solid ${colors.grey[100]}`} borderRadius="4px">
+                <GeographyChart/>
             </Box>
         </Box>
     )
